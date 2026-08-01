b7786ea8621b5e5f975bb060485c2c8660960459
the-app-artisan <theappartisan.dev@gmail.com>
2026-08-01 13:40:49 +0400

    Create Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('It works!');
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send('Hello from Express with TypeScript!');
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
